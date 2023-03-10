f795993c87986260c753f813d1a154b82dc04151
Suganya <ssuganya.nirmal@gmail.com>
2023-03-09 21:29:12 -0800

    first commit

---
+++ src/gitdemo1/firstcode.java
@@ -0,0 +1,12 @@
+package gitdemo1;
+
+public class firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Git");
+		System.out.println("Welcome to Git");
+	}
+
+}
